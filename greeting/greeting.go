14d3d4909ab57fb83fde5fa11cd4c0ec23401466
Evgeny <evg65b@gmail.com>
2026-09-25 09:41:38 +0300

    Fix: proper tabs in Go files

--- greeting/greeting.go
+++ greeting/greeting.go
@@ -3,13 +3,13 @@ package greeting
 import "fmt"
 
 func Greet(name string) string {
-return fmt.Sprintf("Hello, %s!", name)
+	return fmt.Sprintf("Hello, %s!", name)
 }
 
 func SumRange(from, to int) int {
-sum := 0
-for i := from; i <= to; i++ {
-sum += i
-}
-return sum
+	sum := 0
+	for i := from; i <= to; i++ {
+		sum += i
+	}
+	return sum
 }
--- greeting/greeting_test.go
+++ greeting/greeting_test.go
@@ -3,17 +3,17 @@ package greeting
 import "testing"
 
 func TestGreet(t *testing.T) {
-got := Greet("GitHub")
-want := "Hello, GitHub!"
-if got != want {
-t.Errorf("Greet() = %q, want %q", got, want)
-}
+	got := Greet("GitHub")
+	want := "Hello, GitHub!"
+	if got != want {
+		t.Errorf("Greet() = %q, want %q", got, want)
+	}
 }
 
 func TestSumRange(t *testing.T) {
-got := SumRange(1, 10)
-want := 55
-if got != want {
-t.Errorf("SumRange(1, 10) = %d, want %d", got, want)
-}
+	got := SumRange(1, 10)
+	want := 55
+	if got != want {
+		t.Errorf("SumRange(1, 10) = %d, want %d", got, want)
+	}
 }
